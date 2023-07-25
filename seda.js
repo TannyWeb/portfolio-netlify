36c461c00fe71c557ede30facc7e2a3b210df893
Tanny <tanvir1996@yahoo.co.uk>
2023-07-25 23:39:31 +0100

    Update seda.js

--- seda.js
+++ seda.js
@@ -8,10 +8,12 @@ heart.addEventListener('click', () => {
   setTimeout(() => {
     
     
-    if(hiddenMessage.classList.contains('show')) {
-      hiddenMessage.classList.remove('show')
+    if(hiddenMessage.classList.contains('animate__animated')) {
+      hiddenMessage.classList.remove('animate__animated');
+      hiddenMessage.classList.remove('animate__heartBeat');
     } else {
-      hiddenMessage.classList.add('show')
+      hiddenMessage.classList.add('animate__animated', 'animate__heartBeat');
+      hiddenMessage.classList.remove('hideEl')
     }
     
   }, 250);
@@ -19,7 +21,9 @@ heart.addEventListener('click', () => {
   setTimeout(() => {
     if(!heart.classList.contains('grow')) return;
     heart.classList.remove('grow')
-    hiddenMessage.classList.remove('show')
+    hiddenMessage.classList.add('hideEl');
+    hiddenMessage.classList.remove('animate__animated');
+    hiddenMessage.classList.remove('animate__heartBeat');
   }, 5000);
 })
 
@@ -30,66 +34,147 @@ let ticking = false;
 function doAnimation(scrollPos) {
   // Do something with the scroll position
   const tapHeartText = document.querySelector('.hold h3');
+
+
   const memory1 = document.querySelector('.memory-1')
   const memory2 = document.querySelector('.memory-2')
   const memory3 = document.querySelector('.memory-3')
   const memory4 = document.querySelector('.memory-4')
   const memory5 = document.querySelector('.memory-5')
+  const memory6 = document.querySelector('.memory-6')
   const item1 = document.querySelector('.grid .item-1');
   const item2 = document.querySelector('.grid .item-2');
   const item3 = document.querySelector('.grid .item-3');
   const item4 = document.querySelector('.grid .item-4');
   const footerHeart = document.querySelector('.footer svg');
-  if(window.scrollY > 400) {
+
+
+  if(window.innerWidth < 900 && window.scrollY > 400) {
     tapHeartText.classList.add('animate__animated', 'animate__backInUp')
+    tapHeartText.classList.remove('hideEl')
+  }else if(window.innerWidth > 900 && window.scrollY > 400) {
+    tapHeartText.classList.add('animate__animated', 'animate__backInUp')
+    tapHeartText.classList.remove('hideEl')
+  } else {
     tapHeartText.classList.remove('hideEl')
   }
 
-  if(window.scrollY > 1100) {
+
+
+  if(window.innerWidth < 900 && window.scrollY > 1100) {
     memory1.classList.add('animate__animated', 'animate__fadeInUpBig')
     memory1.classList.remove('hideEl')
+  } else if(window.innerWidth > 900 && window.scrollY > 1000) {
+    memory1.classList.add('animate__animated', 'animate__fadeInUpBig')
+    memory1.classList.remove('hideEl')
+  } else {
+    memory1.classList.remove('hideEl')
   }
-  if(window.scrollY > 1700) {
+
+  if(window.innerWidth < 900 && window.scrollY > 1700) {
     memory2.classList.add('animate__animated', 'animate__fadeInUpBig')
     memory2.classList.remove('hideEl')
+  }else if(window.innerWidth > 900 && window.scrollY > 1000) {
+    memory2.classList.add('animate__animated', 'animate__fadeInUpBig')
+    memory2.classList.remove('hideEl')
+  } else {
+    memory2.classList.remove('hideEl')
   }
-  if(window.scrollY > 2300) {
+
+  if(window.innerWidth < 900 && window.scrollY > 2300) {
     memory3.classList.add('animate__animated', 'animate__fadeInUpBig')
+    memory3.classList.remove('hideEl')
+  }else if(window.innerWidth > 900 && window.scrollY > 1500) {
+    memory3.classList.add('animate__animated', 'animate__fadeInUpBig')
+    memory3.classList.remove('hideEl')
+  } else {
     memory3.classList.remove('hideEl')
   }
 
-  if(window.scrollY > 2900) {
+
+  if(window.innerWidth < 900 && window.scrollY > 2900) {
     memory4.classList.add('animate__animated', 'animate__fadeInUpBig')
+    memory4.classList.remove('hideEl')
+  }else if(window.innerWidth > 900 && window.scrollY > 1500) {
+    memory4.classList.add('animate__animated', 'animate__fadeInUpBig')
+    memory4.classList.remove('hideEl')
+  } else {
     memory4.classList.remove('hideEl')
   }
 
+
   
-  if(window.scrollY > 3550) {
+  if(window.innerWidth < 900 && window.scrollY > 3550) {
     memory5.classList.add('animate__animated', 'animate__fadeInUpBig')
     memory5.classList.remove('hideEl')
+  }else if(window.innerWidth > 900 && window.scrollY > 2000) {
+    memory5.classList.add('animate__animated', 'animate__fadeInUpBig')
+    memory5.classList.remove('hideEl')
+  } else {
+    memory5.classList.remove('hideEl')
   }
+  if(window.innerWidth < 900 && window.scrollY > 4000) {
+    memory6.classList.add('animate__animated', 'animate__fadeInUpBig')
+    memory6.classList.remove('hideEl')
+  }else if(window.innerWidth > 900 && window.scrollY > 2000) {
+    memory6.classList.add('animate__animated', 'animate__fadeInUpBig')
+    memory6.classList.remove('hideEl')
+  } else {
+    memory6.classList.remove('hideEl')
+  }
+  
 
-  if(window.scrollY > 4650) {
+  if(window.innerWidth < 900 && window.scrollY > 5700) {
     item1.classList.add('animate__animated', 'animate__zoomIn')
     item1.classList.remove('hideEl')
+  }else if(window.innerWidth > 900 && window.scrollY > 2600) {
+    item1.classList.add('animate__animated', 'animate__zoomIn')
+    item1.classList.remove('hideEl')
+  } else {
+    memory1.classList.remove('hideEl')
   }
 
-  if(window.scrollY > 4950) {
+
+  if(window.innerWidth < 900 && window.scrollY > 6000) {
     item2.classList.add('animate__animated', 'animate__zoomIn')
     item2.classList.remove('hideEl')
   }
-  if(window.scrollY > 5250) {
+  else if(window.innerWidth > 900 && window.scrollY > 2600) {
+    item2.classList.add('animate__animated', 'animate__zoomIn')
+    item2.classList.remove('hideEl')
+  } else {
+    memory1.classList.remove('hideEl')
+  }
+  if(window.innerWidth < 900 && window.scrollY > 6250) {
     item3.classList.add('animate__animated', 'animate__zoomIn')
     item3.classList.remove('hideEl')
   }
-  if(window.scrollY > 5550) {
+  else if(window.innerWidth > 900 && window.scrollY > 2830) {
+    item3.classList.add('animate__animated', 'animate__zoomIn')
+    item3.classList.remove('hideEl')
+  } else {
+    memory1.classList.remove('hideEl')
+  }
+  if(window.innerWidth < 900 && window.scrollY > 6550) {
     item4.classList.add('animate__animated', 'animate__zoomIn')
     item4.classList.remove('hideEl')
   }
+  else if(window.innerWidth > 900 && window.scrollY > 2830) {
+    item4.classList.add('animate__animated', 'animate__zoomIn')
+    item4.classList.remove('hideEl')
+  } else {
+    memory1.classList.remove('hideEl')
+  }
 
-  if(window.scrollY > 5730) {
+  if(window.innerWidth < 900 && window.scrollY > 6900) {
     footerHeart.classList.add('animate__animated', 'animate__bounce')
     footerHeart.classList.remove('hideEl')
+  }
+  else if(window.innerWidth > 900 && window.scrollY > 3000) {
+    footerHeart.classList.add('animate__animated', 'animate__bounce')
+    footerHeart.classList.remove('hideEl')
+  } else {
+    memory1.classList.remove('hideEl')
   }
 
 
